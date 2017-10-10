92fd34d15fee89d00b418c6245483f2455d94b18
bvasilop <bvasilop@gmail.com>
2017-10-09 22:14:17 -0700

    added script config to NewsList.vue

---
+++ vuenews/src/components/NewsList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class ="newslist">
+    Newslist
+  </div>
+
+  </template>
+
+  <script>
+  export default {
+    name: 'newslist'
+  }
+  </script>
+
+  <style scoped>
+
+  </style>
